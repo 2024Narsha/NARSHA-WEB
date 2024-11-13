5856f8af10ac077179b113fc78dc40e04e1a8ecf
Isoye12 <0150371278i@gmail.com>
2024-11-13 23:49:21 +0900

    feat: signup 중간점검

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -5,7 +5,7 @@ import BottomButton from '../../components/BottomButton';
 const Signup = () => {
   return (
     <div>
-      <div top></div>
+      <div></div>
 
       <>이미 계정이 있나요?</><Link to='/login'>로그인</Link>
       <BottomButton title='가입하기' />
