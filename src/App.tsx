ae827182877cb4b89ed3413be45e44670bcdafd9
Isoye12 <0150371278i@gmail.com>
2024-11-19 21:15:46 +0900

    feat :: apply page 끝

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import Footer from "./components/Footer/Footer";
 import { Outlet } from "react-router-dom";
 import ApplyDetail from "./components/ApplyDetail";
 import SongApply from "./pages/SongApply";
+import StoryApply from "./pages/StoryApply";
 
 const Wrapper = () => {
   return (
@@ -46,6 +47,7 @@ const App = () => {
           <Route path="/apply-page" element={<ApplyPage />} />
           <Route path="/previewList" element={<PreviewList />} /> {/* 이거 삭제해야 함 */}
           <Route path="/song-apply" element={<SongApply />} />
+          <Route path="/story-apply" element={<StoryApply />} />
 
         </Route>
 
