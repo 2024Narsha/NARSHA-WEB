b12a86c550571e5597732d9a53bb289118e9935d
aviation5770 <aviation5770@gmail.com>
2024-11-19 19:55:43 +0900

    카테고리 다중선택 지원

--- src/pages/CategoryList/CategoryList.tsx
+++ src/pages/CategoryList/CategoryList.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './CategoryList.css'
+import './CategoryList.css';
 
 const CategorySearch = () => {
   return (
@@ -13,41 +13,47 @@ const CategorySearch = () => {
 };
 
 const CategoryList = () => {
-  const [selectedCategory, setSelectedCategory] = useState<string>('');
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]); // 선택된 카테고리들을 배열로 저장
   const navigate = useNavigate();
 
   // 카테고리 버튼 클릭 시 상태 업데이트
   const handleCategoryClick = (category: string) => {
-    setSelectedCategory(category);
+    if (selectedCategories.includes(category)) {
+      setSelectedCategories(selectedCategories.filter((item) => item !== category));
+    } else {
+      setSelectedCategories([...selectedCategories, category]);
+    }
   };
 
   const handleSearchClick = () => {
-    if (!selectedCategory) {
-      alert('카테고리를 선택해주세요');
+    if (selectedCategories.length === 0) {
+      alert('카테고리를 하나 이상 선택해주세요');
     } else {
-      navigate(`/category-results/${selectedCategory}`);
+      navigate(`/category-results/${selectedCategories.join(',')}`);
     }
   };
 
   return (
-    <div className='centered-container'>
+    <div className="centered-container">
       <CategorySearch />
 
       <h2 className="category-title">대회 분야</h2>
-      
+
       <div className="category-buttons">
-        {['공통', '정보 보안', '아이디어톤', '게임', '해커톤', '프로그래밍', '머신러닝', '로봇', '웹 개발', '앱 개발', 'IoT', '블록체인', '데이터 분석', '디자인', '빅데이터', '컴퓨터 비전', '소프트웨어 설계', '클라우드 개발', '오픈소스', '창업'].map((category) => (
+        {[
+          '공통', '정보 보안', '아이디어톤', '게임', '해커톤', '프로그래밍', '머신러닝', '로봇', 
+          '웹 개발', '앱 개발', 'IoT', '블록체인', '데이터 분석', '디자인', '빅데이터', '컴퓨터 비전', 
+          '소프트웨어 설계', '클라우드 개발', '오픈소스', '창업'
+        ].map((category) => (
           <button
             key={category}
-            className={`category-button ${selectedCategory === category ? 'selected' : ''}`}
+            className={`category-button ${selectedCategories.includes(category) ? 'selected' : ''}`}
             onClick={() => handleCategoryClick(category)}
           >
             {category}
           </button>
         ))}
       </div>
-
-      {/* 검색하기 버튼 */}
       <button className="search-button" onClick={handleSearchClick}>
         검색하기
       </button>
