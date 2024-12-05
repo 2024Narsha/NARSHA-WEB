40a7e4d473760320458473e48bf42c776cfff7f4
Isoye12 <0150371278i@gmail.com>
2024-12-05 17:05:31 +0900

    feat :: auth api

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,27 +1,88 @@
 import './style.css'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import BottomButton from '../../components/BottomButton';
-import InputBox from '../../components/InputBox';
+import { useState } from 'react';
+import axios from 'axios';
 
 const Login = () => {
+
+  const [userId, setUserId] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleUserId = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserId(e.target.value);
+  };
+
+  const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const navigate = useNavigate();
+  
+  const submit = async() => {
+    try{
+      const res = await axios.post(
+        `${import.meta.env.VITE_SERVER_URL}/auth/login`,
+        {userId, password}
+        );
+        if(res){
+          const { accessToken, refreshToken } = res.data;
+          localStorage.setItem('ACCESS_TOKEN',accessToken);
+          localStorage.setItem('REFRASH_TOKEN',refreshToken);
+          alert('로그인 완료');
+          navigate('/')
+        }
+    }catch(error:any){
+      if(error.response.status === 404){
+        alert('아이디가 존재하지 않음!')
+        return
+      }
+      if(error.response.status === 400){
+        alert('정보가 올바르지 않음!')
+        return
+      }
+      alert('네트워크 에러')
+    }
+  }
+
   return (
     <div className='login-page-container'>
       <div className='top-line'></div>
-      <img className='login-page-logo' src={`/logo(blue).png`} />
+      <img className='login-page-logo' src={`/logo-blue.png`} />
 
-      <img className='login-page-title' src={`/logo(text).png`} />
+      <img className='login-page-title' src={`/logo-text.png`} />
       <div className='page-info'>
         수상한 고래에 로그인하여 최신 대회<br></br>
         정보들을 손쉽게 알아보세요!
       </div>
 
       <div className='login-input-box-wrap'>
-        <InputBox label='아이디' placeholder='아이디 입력' />
-        <InputBox label='비밀번호' placeholder='비밀번호 입력' />
+      <div>
+          <p className='label'>아이디</p>
+          <input 
+            className='input-box' 
+            placeholder='아이디 입력' 
+            name='userId' 
+            type='text'
+            onChange={handleUserId}
+            value={userId}
+          />
+        </div>
+        <div>
+          <p className='label'>비밀번호</p>
+          <input 
+            className='input-box' 
+            placeholder='비밀번호 입력' 
+            name='password' 
+            type='password'
+            onChange={handlePassword}
+            value={password}
+          />
+        </div>
         <div className='go-to-login'>계정이 없으신가요? <Link to='/signup' style={{color: '#3F4142'}}>회원가입</Link></div>
       </div>
 
-      <BottomButton title='로그인 하기' />
+      <BottomButton title='로그인 하기' onClick={submit} />
     </div>
   )
 }
--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,13 +1,67 @@
 import "./style.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import BottomButton from "../../components/BottomButton";
-import InputBox from "../../components/InputBox";
+
+import { useState } from "react";
+import axios from "axios";
 
 const Signup = () => {
+  const navigate = useNavigate(); // 네비게이션 훅(페이지 이동)
+
+  const [userId, setUserId] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [checkPassword, setCheckPassword] = useState<string>('');
+
+  const handleUserId = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserId(e.target.value);
+  };
+
+  const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleCheckPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCheckPassword(e.target.value);
+  };
+
+  const submit = async() => {
+    if(userId !== userId.replace(/\s+/g, '')){
+      alert('아이디에 공백이 있음!');
+      return
+    };
+
+    if(password !== password.replace(/\s+/g, '')){
+      alert('비밀번호에 공백이 있음!');
+      return
+    };
+
+    if(password !== checkPassword){
+      alert('비밀번호가 일치하지 않음!');
+      return
+    };
+
+    try{
+      const res = await axios.post(
+        `${import.meta.env.VITE_SERVER_URL}/auth/signup`, 
+        {userId, password}
+      );
+        if(res){
+          alert('회원가입 완료');
+          navigate('/login')
+        };
+      }catch(error: any){
+        if(error.response.status === 409){
+          alert('이미 사용 중인 아이디!');
+          return
+        }
+        alert('네트워크 에러');
+      };
+    };
+
   return (
     <div className="signup-page-container">
       <div className="top-line"></div>
-      <img src={`/logo(blue).png`} />
+      <img src={`/logo-blue.png`} />
       <div className="signup-page-title">회원가입</div>
       <div className="page-info">
         수상한 고래를 이용하기 전,<br></br>
@@ -15,13 +69,46 @@ const Signup = () => {
       </div>
 
       <div className="signup-input-box-wrap">
-        <InputBox label="아이디" placeholder="아이디 입력" />
-        <InputBox label="비밀번호" placeholder="비밀번호 입력" />
-        <InputBox label="비밀번호 확인" placeholder="비밀번호 확인" />
+        <div>
+          <p className='label'>아이디</p>
+          <input 
+            className='input-box' 
+            placeholder='아이디 입력' 
+            name='userId' 
+            type='text' 
+            onChange={handleUserId} 
+            value={userId} 
+          />
+        </div>
+        <div>
+          <p className='label'>비밀번호</p>
+          <input 
+            className='input-box' 
+            placeholder='비밀번호 입력' 
+            name='password' 
+            type='password' 
+            onChange={handlePassword} 
+            value={password} 
+          />
+        </div>
+        <div>
+          <p className='label'>비밀번호 확인</p>
+          <input 
+            className='input-box' 
+            placeholder='비밀번호 확인' 
+            name='checkPassword' 
+            type='password' 
+            onChange={handleCheckPassword} 
+            value={checkPassword}
+          />
+        </div>
         <div className="go-to-login">이미 계정이 있나요? <Link to="/login" style={{ color: "#3F4142" }}>로그인</Link></div>
       </div>
 
-      <BottomButton title="가입하기" />
+      <BottomButton 
+        title="가입하기"
+        onClick={submit} 
+       />
     </div>
   );
 };
